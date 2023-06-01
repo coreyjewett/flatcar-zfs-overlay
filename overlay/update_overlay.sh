bff41f7fafe18496a36eb7ed765b4565097e1252
Corey Jewett <cj@syntheticplayground.com>
2023-06-01 12:38:00 -0700

    update eclass to match current portage

--- overlay/update_overlay.sh
+++ overlay/update_overlay.sh
@@ -5,4 +5,5 @@ find sys-fs -type f -exec rm {} \;
 
 # grab the tarball and extract the relevant packages
 curl -L 'https://github.com/gentoo/gentoo/tarball/master' | tar xz --strip-components 1 \
-    $(find sys-fs -maxdepth 1 -mindepth 1 -exec echo \*/{} \;)
+    $(find sys-fs -maxdepth 1 -mindepth 1 -exec echo \*/{} \;) \
+    \*/eclass/dist-kernel-utils.eclass
